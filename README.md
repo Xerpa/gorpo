9afb0486ac1fc6c914c11cd4c1bce5cd3feebe64
dgvncsz0f <dsouza@c0d3.xxx>
2016-12-04 15:38:40 -0200

    travis.yml file

---
+++ README.md
@@ -0,0 +1,4 @@
+# gorpo
+
+gorpo is a service discovery service for elixir that uses
+Consul. ![build status](https://travis-ci.org/Xerpa/gorpo.svg?branch=master)
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gorpo.Mixfile do
   def project do
     [app: :gorpo,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: ">= 1.2.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
@@ -18,8 +18,8 @@ defmodule Gorpo.Mixfile do
 
   defp deps do
     [{:poison, ">= 1.2.0"},
-     {:dialyxir, "~> 0.4", only: [:dev]},
-     {:ex_doc, "~> 0.13", only: [:dev]}]
+     {:dialyxir, ">= 0.4.0", only: [:dev]},
+     {:ex_doc, ">= 0.14.3", only: [:dev]}]
   end
 
 end
