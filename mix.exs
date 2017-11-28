91e9692be1c2cc86e634e9744858d6feeecdb677
Charlotte Lorelei Oliveira <11341349+mememori@users.noreply.github.com>
2017-11-28 12:57:55 -0200

    Add package data to mix

--- mix.exs
+++ mix.exs
@@ -1,26 +1,45 @@
 defmodule Gorpo.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
-    [app: :gorpo,
-     version: "0.1.0",
-     elixir: ">= 1.2.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     dialyzer: [plt_add_apps: [:poison, :inets],
-                flags: [:unmatched_returns, :error_handling]]]
+    [
+      app: :gorpo,
+      version: @version,
+      elixir: ">= 1.2.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+
+      dialyzer: [plt_add_apps: [:poison, :inets]],
+
+      description: "Service discovery using Consul",
+      package: package()
+    ]
   end
 
   def application do
-    [mod: {Gorpo, []},
-     applications: [:logger]]
+    [
+      mod: {Gorpo, []},
+      applications: [:logger]
+    ]
   end
 
   defp deps do
-    [{:poison, ">= 1.2.0"},
-     {:dialyxir, ">= 0.5.0", only: [:dev], runtime: false},
-     {:ex_doc, ">= 0.14.3", only: [:dev]}]
+    [
+      {:poison, ">= 1.2.0"},
+      {:dialyxir, ">= 0.5.0", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.14.3", only: :dev}
+    ]
   end
 
+  defp package do
+    [
+      maintainers: ["Diego Vinicius e Souza"],
+      licenses: ["BSD 2-clause"],
+      links: %{"GitHub" => "https://github.com/Xerpa/gorpo"},
+      files: ["lib", "mix.exs", "README.md", "COPYING"]
+    ]
+  end
 end
